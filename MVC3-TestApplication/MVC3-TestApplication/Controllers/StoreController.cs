cee6a10985f9ff694644652f6be64a51dd4e8847
Francisco Fiorillo <francisco.fiorillo@razorfish.com>
2013-06-06 11:06:33 -0300

    Added MVC project files

---
+++ MVC3-TestApplication/MVC3-TestApplication/Controllers/StoreController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC3_TestApplication.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+        public String Index()
+        {
+            //return View();
+            return "Welcome to the jungle! From Store.Index()";
+        }
+
+        // GET: /Store/Browse
+        public String Browse(String id)
+        {
+            //return View();
+            if (String.IsNullOrEmpty(id))
+                return "Browsing all genres (no genre specified)";
+            return HttpUtility.HtmlEncode("Browsing music library... Genre: " + id);
+        }
+
+        // GET: /Store/Details
+        public String Details(String strDetail)
+        {
+            //return View();
+            return HttpUtility.HtmlEncode("Welcome to the details! From Store.Details(). This detail was added: " + strDetail);
+        }
+
+    }
+}
---
+++ MVC3-TestApplication/MVC3-TestApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC3_TestApplication.MvcApplication" Language="C#" %>
