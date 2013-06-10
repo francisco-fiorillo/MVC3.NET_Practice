ae5abe7248bc399ce485e8ca21a537d49e2584dc
Francisco Fiorillo <panchof28@yahoo.com.ar>
2013-06-10 17:16:00 -0300

    Added Search functionality
    (Page 98)

--- Mvc3-Scaffolding/Mvc3-Scaffolding/Controllers/HomeController.cs
+++ Mvc3-Scaffolding/Mvc3-Scaffolding/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Mvc3_Scaffolding.Models;
 
 namespace Mvc3_Scaffolding.Controllers
 {
@@ -13,6 +14,16 @@ namespace Mvc3_Scaffolding.Controllers
             ViewBag.Message = "Welcome to ASP.NET MVC!";
 
             return View();
+        }
+
+        public ActionResult Search(String q)
+        {
+            MusicStoreDB storeDB = new MusicStoreDB();
+            var albums = storeDB.Albums
+                                .Include("Artist")
+                                .Where(a => a.Title.Contains(q) || q == null)
+                                .Take(10);
+            return View(albums);
         }
 
         public ActionResult About()
